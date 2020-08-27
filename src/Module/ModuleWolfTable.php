18383dd9264b56aac16e8d7ce57c07b95cc9d18c
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-08-27 12:09:35 +0200

    Create ModuleWolfTable.php

---
+++ src/Module/ModuleWolfTable.php
@@ -0,0 +1,29 @@
+<?php
+ 
+class ModuleWolfTable extends Module
+{
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = 'mod_wolftable';
+ 
+public function generate()
+    {
+        if (TL_MODE == 'BE')
+        {
+            $objTemplate = new \BackendTemplate('be_wildcard');
+            $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['wolftable'][0]) . ' ###';
+            //$objTemplate->firma = $this->headline;
+            return $objTemplate->parse();
+        }
+    return parent::generate();
+    }
+	
+	/**
+	 * Compile the current element
+	 */
+	protected function compile()
+	{
+	}
+}
