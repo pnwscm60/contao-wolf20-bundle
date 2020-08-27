ddb06c7ffd031f04ff21c7aea257562470c635ea
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-08-27 13:17:38 +0200

    Create tl_module.php

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Wolf System
+ *
+ * Copyright (c) 2020 Markus Schenker
+ */
+
+
+/**
+ * Add palettes to tl_module
+ */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolftable']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfgraph']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfentry0']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfentry1']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfresults']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfreview']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfinstedit']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfprofil']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfadmin']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wolfsdo']    = '{title_legend},name,headline,type;{config_legend},{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+?>
+
+
+
+
