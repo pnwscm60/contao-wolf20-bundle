f727e7575fcbfbe96c5b48e9685112291ae7bdc4
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-08-27 13:13:36 +0200

    Create tl_sdoimages.php

---
+++ src/Resources/contao/dca/tl_sdoimages.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+DCA für wolfinstitute
+© 2017 Markus Schenker, Phi Network
+ */
+
+
+/**
+ * Table tl_sdoimages
+ * Table for wolf_original_sunspot-data
+ */
+$GLOBALS['TL_DCA']['tl_sdoimages'] = array
+(
+	// Config
+	'config' => array
+	(
+		'dataContainer'               => 'Table',
+		'enableVersioning'            => true,
+		'sql' => array
+		(
+			'keys' => array
+			(
+				'id' => 'primary',
+			)
+		),
+	),
+
+	// List
+	'list' => array
+	(
+		'sorting' => array
+		(
+			'mode'                    => 2,
+			'fields'                  => array('sorting'),
+			'headerFields'			=> array('sdo_name'),
+			'panelLayout'             => 'filter;sort,search,limit',
+		),
+		'label' => array
+		(
+			'fields'                  => array('sdo_name'),
+			'format'                  => '%s',
+		),
+		'global_operations' => array
+		(
+			'all' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+				'href'                => 'act=select',
+				'class'               => 'header_edit_all',
+				'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+			)
+		),
+		'operations' => array
+		(
+			'edit' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_sdoimages']['edit'],
+				'href'                => 'act=edit',
+				'icon'                => 'edit.gif'
+			),
+			'delete' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_sdoimages']['delete'],
+				'href'                => 'act=delete',
+				'icon'                => 'delete.gif',
+				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+			),
+			/*'toggle' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_sdoimages']['toggle'],
+				'icon'                => 'visible.gif',
+				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+				'button_callback'     => array('tl_sdoimages', 'toggleIcon')
+			),*/
+			'show' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_sdoimages']['show'],
+				'href'                => 'act=show',
+				'icon'                => 'show.gif',
+				'attributes'          => 'style="margin-right:3px"'
+			),
+		)
+	),
+
+	// Palettes
+	'palettes' => array
+	(
+		'default'	=> 'sdo_name'
+	),
+
+	// Fields
+	'fields' => array
+	(
+		'id' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+		),
+		'sorting' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),'tstamp' => array
+		(
+			'sql'                     => "timestamp NOT NULL default CURRENT_TIMESTAMP"
+		),
+		/*'alias' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_sdoimages']['alias'],
+			'exclude'                 => true,
+			'search'                  => true,
+			'inputType'               => 'text',
+			'eval'                    => array('rgxp'=>'alias', 'unique'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+			'save_callback' => array
+			(
+				array('tl_sdoimages', 'generateAlias')
+			),
+			'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
+		),*/
+	
+		'sdo_name' => array
+		(
+			'label'				=> &$GLOBALS['TL_LANG']['tl_sdoimages']['sdo_name'],
+			'exclude'			=> false,
+			'search'			=> true,
+			'sorting'			=> true,
+			'filter'			=> true,
+			'flag'				=> 11,
+			'inputType'			=> 'text',
+			'eval'				=> array(
+				'mandatory'	=>	true,
+				'alnum'		=>	true,
+				'unique'	=>	true,
+				'tl_class'=>'w50'
+				),
+			'sql'                     => "varchar(20) NOT NULL default ''"
+		),
+	)
+);
+/*Classes*/
+class tl_sdoimages extends Backend {	
+	
+}
