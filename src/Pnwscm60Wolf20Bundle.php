81fcb12c53848186c75d9bbebaad57c62b8a2ffc
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-08-27 09:21:52 +0200

    Create Pnwscm60Wolf20Bundle.php

---
+++ src/Pnwscm60Wolf20Bundle.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is part of Wolf.
+ *
+ * (c) 2020 Markus Schenker
+ *
+ * @license LGPL-3.0-or-later
+ */
+namespace Pnwscm60\Wolf20Bundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+/* Configure the Contao Wolf20 Bundle */
+class Pnwscm60Wolf20Bundle extends Bundle
+{
+}
