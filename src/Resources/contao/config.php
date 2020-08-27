43c05938b0c9004022fa05696cf68743a1a1898b
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-08-27 12:17:20 +0200

    Create config.php

---
+++ src/Resources/contao/config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Back end modules
+ * Front end modules
+ */
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfAdmin;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfEntry0;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfEntry1;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfProfil;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfGraph;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfInstedit;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfResults;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfReview;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfSDO;
+use Pnwscm60\Wolf20Bundle\Module\ModuleWolfTable;
+$GLOBALS['FE_MOD']['wolf20'] = [ 
+	'admin' => ModuleWolfAdmin::class,
+	'entry0' => ModuleWolfEntry0::class,
+	'entry1' => ModuleWolfEntry1::class,
+	'profil' => ModuleWolfProfil::class,
+	'graph' => ModuleWolfGraph::class,
+	'instedit' => ModuleWolfInstedit::class,
+	'results' => ModuleWolfResults::class,
+	'review' => ModuleWolfReview::class,
+  'sdo' => ModuleWolfSDO::class,
+  'table' => ModuleWolfTable::class
+];  
