65358e22436c940905f9268bffe5e373649dd28b
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-09-02 13:51:50 +0200

    Update ModuleWolfInstedit.php

--- src/Module/ModuleWolfInstedit.php
+++ src/Module/ModuleWolfInstedit.php
@@ -13,7 +13,7 @@ public function generate()
         if (TL_MODE == 'BE')
         {
             $objTemplate = new \BackendTemplate('be_wildcard');
-            $objTemplate->wildcard = '### instedit ###';
+            $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['wolfinstedit'][0]) . ' ###';
             //$objTemplate->firma = $this->headline;
             return $objTemplate->parse();
         }
@@ -26,44 +26,24 @@ public function generate()
 	protected function compile()
 	{
 		
-			
+	//DB
+    $db = \Contao\System::getContainer()->get('database_connection');
 	//Daten bereitstellen User > Instruments
         $this->import('FrontendUser', 'User');
         $userid = $this->User->id;
 
 //Informationen zum Observer
-        $sql="SELECT * from tl_member WHERE id = ".$userid;
-        $result = $this->Database->prepare($sql)->execute();
-        $this->Template->observer = $result->id;
-        $this->Template->lname = $result->lastname;
-        $this->Template->fname = $result->firstname;
-        $this->Template->city = $result->city;
-        $this->Template->country = strtoupper($result->country);		
+        $qsql='SELECT * from tl_member WHERE id = ?';
+        $mresult = $db->executeQuery($qsql, array($userid))->fetch();
+        $this->Template->observer = $mresult['id'];
+        $this->Template->lname = $mresult['lastname'];
+        $this->Template->fname = $mresult['firstname'];
+        $this->Template->city = $mresult['city'];
+        $this->Template->country = strtoupper($mresult['country']);	
 
-//Show all instrument for this user
-        $sql="SELECT *, tl_instrument.id as inid from tl_instrument WHERE i_id=".$userid;
-        
-        $result2 = $this->Database->prepare($sql)->execute();
 
-        $instArray = array();
-        while($result2->next())
-        {
-            $instaArray[] = array
-		(
-			'id' => $result2->inid,
-			'i_id' => $result2->i_id,
-            'i_aperture' => $result2->i_aperture,
-            'i_focal_length' => $result2->i_focal_length,
-            'i_filter' => $result2->i_filter,
-            'i_method' => $result2->i_method,
-            'i_magnification' => $result2->i_magnification,
-            'i_projection' => $result2->i_projection,
-            'i_inputpref' => $result2->i_inputpref,
-                );
-        }
-        $this->Template->allinstr = $instaArray;
 		
-		if($_REQUEST['modifyinst']==1){ //Instrument modifizieren!
+if($_REQUEST['modifyinst']==1){ //Instrument modifizieren!
 			if(isset($code)){
 				$instcode=$code;
 			} else {
@@ -75,27 +55,18 @@ public function generate()
 				}
 			}
 			
-		$sql="SELECT *, tl_instrument.id as inid from tl_instrument, tl_member WHERE tl_instrument.id=".$instcode." AND tl_member.id = i_id";
+		$qsql="SELECT *, tl_instrument.id as inid from tl_instrument, tl_member WHERE tl_instrument.id= ?";
         //echo $sql;
-        $result = $this->Database->prepare($sql)->execute();
-
-        $instArray = array();
-        while($result->next())
-        {
-            $instArray[] = array
-		(
-			'id' => $result->inid,
-			'i_id' => $result->i_id,
-            'i_aperture' => $result->i_aperture,
-            'i_focal_length' => $result->i_focal_length,
-            'i_filter' => $result->i_filter,
-            'i_method' => $result->i_method,
-            'i_magnification' => $result->i_magnification,
-            'i_projection' => $result->i_projection,
-            'i_inputpref' => $result->i_inputpref,
-                );
-        }
-        $this->Template->instr = $instArray;
+        $result = $db->executeQuery($qsql, array($instcode))->fetch();
+			$this->Template->id = $result['inid'];
+			$this->Template->i_id = $result['i_id'];
+            $this->Template->i_aperture = $result['i_aperture'];
+            $this->Template->i_focal_length = $result['i_focal_length'];
+            $this->Template->i_filter = $result['i_filter'];
+            $this->Template->i_method = $result['i_method'];
+            $this->Template->i_magnification = $result['i_magnification'];
+            $this->Template->i_projection = $result['i_projection'];
+            $this->Template->i_inputpref = $result['i_inputpref'];
 		$this->Template->modifyinst = 1;	
 		}
         
@@ -104,52 +75,42 @@ public function generate()
 			
 			$icode = $_REQUEST['icode'];
 			$ispez = $_REQUEST['iakt'];
-			//echo $icode."/".$ispez."<br/>";
-			$sql = "UPDATE tl_instrument set i_inputpref = ".$ispez." WHERE id=".$icode;
-			//echo $sql."<br/>";
-			$result = $this->Database->prepare($sql)->execute();
-			$sql="SELECT *, tl_instrument.id as inid from tl_instrument,  tl_member WHERE tl_instrument.id=".$icode." AND tl_member.id = i_id";
-			//echo $sql;
-			 $result = $this->Database->prepare($sql)->execute();
-
-        $instArray = array();
-        while($result->next())
-        {
-            $inst2Array[] = array
-		(
-			'id' => $result->inid,
-			'i_id' => $result->i_id,
-            'i_aperture' => $result->i_aperture,
-            'i_focal_length' => $result->i_focal_length,
-            'i_filter' => $result->i_filter,
-            'i_method' => $result->i_method,
-            'i_magnification' => $result->i_magnification,
-            'i_projection' => $result->i_projection,
-            'i_inputpref' => $result->i_inputpref,
-                );
-        }
-        $this->Template->instr = $inst2Array;
+            $db->update('tl_instrument', array('i_inputpref' => $ispez), array('id' => $icode));
+                
+			//$result = $this->Database->prepare($sql)->execute();
+			$qsql="SELECT *, tl_instrument.id as inid from tl_instrument, tl_member WHERE tl_instrument.id= ?";
+        $result = $db->executeQuery($qsql, array($icode))->fetch();
+			$this->Template->id = $result['inid'];
+			$this->Template->i_id = $result['i_id'];
+            $this->Template->i_aperture = $result['i_aperture'];
+            $this->Template->i_focal_length = $result['i_focal_length'];
+            $this->Template->i_filter = $result['i_filter'];
+            $this->Template->i_method = $result['i_method'];
+            $this->Template->i_magnification = $result['i_magnification'];
+            $this->Template->i_projection = $result['i_projection'];
+            $this->Template->i_inputpref = $result['i_inputpref'];
 		$this->Template->safeinstmode = 1;	
 		}
         
 		if($_REQUEST['viewinst']==1){
-            $sql1='SELECT *, tl_instrument.id as inid from tl_instrument, tl_member WHERE i_id = '.$userid.' AND i_id=tl_member.id ORDER BY tl_instrument.id';
-			$result = $this->Database->prepare($sql)->execute();
+            $qsql='SELECT *, tl_instrument.id as inid from tl_instrument, tl_member WHERE tl_member.id = ? AND i_id = tl_member.id ORDER BY tl_instrument.id';
+			$resultall2 = $db->executeQuery($qsql, array($userid))->fetchAll();
+            
             $instArray = array();
-            while($result->next())
+            foreach($resultall2 as $result)
         {
             $inst3Array[] = array
 		(
-			'id' => $result->inid,
-			'i_id' => $result->i_id,
-            'i_type' => $result->i_type,
-            'i_aperture' => $result->i_aperture,
-            'i_focal_length' => $result->i_focal_length,
-            'i_filter' => $result->i_filter,
-            'i_method' => $result->i_method,
-            'i_magnification' => $result->i_magnification,
-            'i_projection' => $result->i_projection,
-            'i_inputpref' => $result->i_inputpref,
+			'id' => $result['inid'],
+			'i_id' => $result['i_id'],
+            'i_type' => $result['i_type'],
+            'i_aperture' => $result['i_aperture'],
+            'i_focal_length' => $result['i_focal_length'],
+            'i_filter' => $result['i_filter'],
+            'i_method' => $result['i_method'],
+            'i_magnification' => $result['i_magnification'],
+            'i_projection' => $result['i_projection'],
+            'i_inputpref' => $result['i_inputpref'],
                 );
         }
         $this->Template->instr = $inst3Array;
@@ -179,7 +140,7 @@ public function generate()
 			if($_REQUEST['mag']){
 				$mag=$_REQUEST['mag'];
 			}
-			if($_REQUEST['project']){
+			if($_REQUEST['project']||$_REQUEST['project']==0){
 				$project=$_REQUEST['project'];
 			}
 			if($_REQUEST['input']){
@@ -188,54 +149,82 @@ public function generate()
 			if($_REQUEST['id']){
 				$id=$_REQUEST['id'];	
 			}
-
-			$sql='INSERT into tl_instrument (i_id, i_type, i_aperture, i_focal_length, i_filter, i_method, i_magnification, ';
-			if($_REQUEST['project']!=''){
+            $db->insert('tl_instrument', array('i_id' => $userid, 'i_type' => $type, 'i_aperture' => $aperture, 'i_focal_length' => $focal, 'i_filter' => $filter, 'i_method' => $method, 'i_magnification' => $mag, 'i_projection' => $project));
+			/*$sql='INSERT into tl_instrument (i_id, i_type, i_aperture, i_focal_length, i_filter, i_method, i_magnification, ';
+			if(isset($project)){
 				$sql.='i_projection, ';
 			} 
 			$sql.='i_inputpref) VALUES ('.$userid.', "'.$type.'", '.$aperture.', '.$focal.', "'.$filter.'", "'.$method.'", '.$mag.', ';
-			if($_REQUEST['project']!=''){
+			if(isset($project)){
 				$sql.=$project.',';
 			}
 			$sql.= '0);'; 
-
-
+*/
+            //echo $project;    
 			//echo $sql;
-			$doit = mysql_query($sql) or die(mysql_error());
-			$this->newmessage = 'Data of new instr has been saved.';
-			
+			//$result = $this->Database->prepare($sql)->execute();
+			$this->newmessage = 'Data of new instrument has been saved.';
+			$this->Template->newinst = 1;
+            $this->Template->savenewinst = 1;
 		}
+        
+        
 		if($_REQUEST['instexcel']==1){
 			ob_end_clean();
 			// sql-statement
-			$sql='SELECT * from tl_instrument WHERE i_id = '.$userid.' ORDER by id ASC;';
-
+			$csql='SELECT * from tl_instrument WHERE i_id = ? ORDER by id ASC;';
+			$resultcsv = $db->executeQuery($csql, array($userid))->fetchAll();
+            
 			//echo $sql.'<br/><br/>';
-			$result=mysql_query($sql) or die(mysql_error());
-			$num_fields = mysql_num_fields($result);
-
+			//$result=mysql_query($sql) or die(mysql_error());
+			
 			// Filename with current date
 			$current_date = date("y/m/d");
-					$filename = "Instruments_".$current_date.".xls";
+					$filename = "Instruments_".$mresult['lastname']."_".$current_date.".csv";
 
 			// Open php output stream and write headers
 			$fp = fopen('php://output', 'w');
-			if ($fp && $result) {
-			header("Content-Type: application/vnd.ms-excel; charset=utf-8");
-			header("Content-Disposition: attachment; filename=".$filename);  header("Expires: 0");
-			header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-			header("Cache-Control: private",false);
+            header('Content-Type: text/csv; charset=utf-8');
+            header("Content-Disposition: attachment; filename=".$filename);  header("Expires: 0");
+			//if ($fp && $resultcsv) {
+			//header("Content-Type: application/vnd.ms-excel; charset=utf-8");
+			//header("Content-Disposition: attachment; filename=".$filename);  header("Expires: 0");
+			//header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+			//header("Cache-Control: private",false);
 
 				//Kopfzeile schreiben
-				echo "code \t user \t type \t apert \t foc.l \t filter \t method \t magn \t proj \t input \n";
-				//Zeilen ausgeben
-				while ($r = mysql_fetch_row($result)) {
+				//echo "code \t user \t type \t apert \t foc.l \t filter \t method \t magn \t proj \t input \n";
+				echo "code;user;type;apert;foc.l;filter;method;magn;proj;inputpref\n";
+                //Zeilen ausgeben
+				foreach($resultcsv as $r) {
 				//$row_tally = $row_tally + 1;
-				echo $r[0]."\t".$r[3]."\t".$r[4]."\t".$r[5]."\t".$r[6]."\t".$r[7]."\t".$r[8]."\t".$r[9]."\t".$r[10]."\t".$r[11]."\n";
+				//echo $r[0]."\t".$r[3]."\t".$r[4]."\t".$r[5]."\t".$r[6]."\t".$r[7]."\t".$r[8]."\t".$r[9]."\t".$r[10]."\t".$r[11]."\n";
+                echo $r['id'].";".$r['i_id'].";".$r['i_type'].";".$r['i_aperture'].";".$r['i_focal_length'].";".$r['i_filter'].";".$r['i_method'].";".$r['i_magnification'].";".$r['i_projection'].";".$r['i_inputpref']."\n";
 				}
 
-			}
+			//}
 				die;
 		}
+        
+        //Show all instrument for this user
+        $qsql="SELECT *, tl_instrument.id as inid from tl_instrument WHERE i_id = ?";
+        $resultall = $db->executeQuery($qsql, array($userid))->fetchAll();
+        $instArray = array();
+        foreach($resultall as $result)
+        {
+            $instaArray[] = array
+		(
+			'id' => $result['inid'],
+			'i_id' => $result['i_id'],
+            'i_aperture' => $result['i_aperture'],
+            'i_focal_length' => $result['i_focal_length'],
+            'i_filter' => $result['i_filter'],
+            'i_method' => $result['i_method'],
+            'i_magnification' => $result['i_magnification'],
+            'i_projection' => $result['i_projection'],
+            'i_inputpref' => $result['i_inputpref'],
+                );
+        }
+        $this->Template->allinstr = $instaArray;
 	}
 }
