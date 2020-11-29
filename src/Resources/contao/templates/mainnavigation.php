9d17753baaf84d3d2ba406839e95e31115a1d60f
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-11-30 00:02:39 +0100

    Create mainnavigation.php

---
+++ src/Resources/contao/templates/mainnavigation.php
@@ -0,0 +1,233 @@
+<!-- PUT THIS FIELD IN TEMPLATE DIRECTORY OF CONTAO -->
+
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-black py-3 sticky-top shadow-sm">
+        <!--<div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+  <!--<a class="navbar-brand" href="#">Mega Dropdown</a>-->
+  <button class="navbar-toggler ml-auto custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon my-toggler"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">HOME</a>
+      </li>
+      <li class="nav-item dropdown megamenu">
+        <a class="nav-link dropdown-toggle" href="#" id="megamenu1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          PUBLIC
+        </a>
+        <div class="dropdown-menu" aria-labelledby="megamenu1" id="megamenu11">
+          <div class="container">
+            <div class="row bg-black rounded-0 ">
+                
+              <div class="col">
+                  <div class="text-uppercase text-white subtit">SDO</div>
+                <!--<span class="text-uppercase text-white"></span>-->
+                <ul class="nav flex-column">
+              
+                <li class="nav-item">
+                  <a class="nav-link" href="sdo.html">CURRENT DATA</a></li></ul>
+              </div>
+              <!-- /.col-md-4  -->
+              <div class="col">
+                  <div class="text-uppercase text-white subtit">SWISS WOLF NUMBERS</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="bulletin.html">MONTHLY BULLETIN</a></li>
+                <li class="nav-item"><a class="nav-link" href="datatable.html">DATA TABLES</a></li>
+                <li class="nav-item"><a class="nav-link" href="rawdata.html">RAW DATA</a></li>
+                    <!--
+                    <li class="nav-item">
+                  <a class="nav-link" href="#">ACTIVITY FORCAST</a>
+                </li>
+                    <li class="nav-item">
+                  <a class="nav-link" href="#">RESULTS</a>
+                </li>
+                    -->
+              </ul>
+              </div>
+                <div class="col">
+                    <div class="text-uppercase text-white subtit">HERITAGE</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="description.html">Description</a></li>
+                <li class="nav-item"><a class="nav-link" href="datatables.html">Data tables</a></li>
+                    <li class="nav-item"><a class="nav-link" href="explanations.html">Explanation</a></li>
+                    <li class="nav-item"><a class="nav-link" href="dynamicgraph.html">Dynamic Graph</a></li>
+                    <li class="nav-item"><a class="nav-link" href="datadownload.html">Downloads</a>
+                </li>
+              </ul>
+              </div>
+                <!-- /.col-md-4  -->
+              <div class="col">
+                  <div class="text-uppercase text-white subtit">Links</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="http://solarpatrol.ch" target="_blank">RoboSOT</a></li>
+                <li class="nav-item"><a class="nav-link" href="https://sdo.gsfc.nasa.gov/data" target="_blank">NASA - SDO</a></li>
+                <li class="nav-item"><a class="nav-link" href="https://www.ngdc.noaa.gov/stp/solar/solardataservices.html" target="_blank">NOAA</a></li>
+                <li class="nav-item"><a class="nav-link" href="http://www.sidc.be/silso/" target="_blank">SILSO</a></li>
+              </ul>
+              <!-- /.col-md-4  -->
+              <!--<div class="col-md-4">
+                <a href="">
+                  <img src="https://dummyimage.com/200x100/ccc/000&text=image+link" alt="" class="img-fluid">
+                </a>
+                <p class="text-white">Short image call to action</p>
+
+              </div>-->
+              <!-- /.col-md-4  -->
+            </div>
+          </div>
+          <!--  /.container  -->
+
+
+        </div>
+      </li>
+      <li class="nav-item dropdown megamenu">
+        <a class="nav-link dropdown-toggle" href="#" id="megamenu2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Institute
+        </a>
+        <div class="dropdown-menu" aria-labelledby="megamenu2" id="megamenu22">
+          <div class="container">
+             <div class="row bg-black rounded-0 ">
+              <div class="col">
+                <div class="text-uppercase text-white subtit">INSTITUTE</div>
+                <ul class="nav flex-column">
+                <li class="nav-item">
+                  <a class="nav-link " href="#">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ACTIVITIES</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">CONTACT</a>
+                </li>
+              </ul>
+              </div>
+              <!-- /.col-md-4  -->
+              <div class="col">
+                  <div class="text-uppercase text-white subtit">PROJECTS</div>
+                <ul class="nav flex-column">
+                <li class="nav-item">
+                  <a class="nav-link " href="#">SOURCE BOOK</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">DATA REDUCTION</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">WOLF SERIES</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">GROUP NUMBER</a>
+                </li>
+              </ul>
+              </div>
+             
+              <!-- /.col-md-4  -->
+            </div>
+          </div>
+          <!--  /.container  -->
+
+
+        </div>
+      </li>
+      <li class="nav-item dropdown megamenu">
+        <a class="nav-link dropdown-toggle" href="#" id="megamenu3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          SOLAR OBSERVATION
+        </a>
+        <div class="dropdown-menu" aria-labelledby="megamenu3" id="megamenu33">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+               <div class="text-uppercase text-white subtit">DATA</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="instrument.html">ENTRY</a></li>
+                <li class="nav-item"><a class="nav-link" href="wolfreview.html">REVIEW</a></li>
+              </ul>
+              </div>
+              <!-- /.col-md-4  -->
+              <div class="col">
+               <div class="text-uppercase text-white subtit">EDIT</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="instedit.html">INSTRUMENT</a></li>
+                <li class="nav-item"><a class="nav-link" href="profil.html">PROFILE</a></li>
+              </ul>
+              </div>
+             <!-- /.col-md-4  -->
+              <div class="col">
+               <div class="text-uppercase text-white subtit">INSTRUCTION</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="hb.wolfinstitute.html">USER GUIDE</a></li>
+                <li class="nav-item"><a class="nav-link" href="webadmin.html">WEBADMIN</a></li>
+              </ul>
+              </div>
+              <!-- /.col-md-4  -->
+              <div class="col">
+               <div class="text-uppercase text-white subtit">OBSERVATION</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="goalmethods.html">GOALS/METHODS</a></li>
+                <li class="nav-item"><a class="nav-link" href="observationprog.html">OBSERVATION PROGRAMS</a></li>
+                <li class="nav-item"><a class="nav-link" href="swisswolfnr.html">SWISS WOLF NUMBERS</a></li>
+                <li class="nav-item"><a class="nav-link" href="references.html">REFERENCES</a></li>
+              </ul>
+              </div>
+            </div>
+          </div>
+          <!--  /.container  -->
+
+
+        </div>
+      </li>
+        
+        <li class="nav-item dropdown megamenu">
+        <a class="nav-link dropdown-toggle" href="#" id="megamenu4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          RUDOLF WOLF SOCIETY
+        </a>
+        <div class="dropdown-menu" aria-labelledby="megamenu4" id="megamenu44">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+               <div class="text-uppercase text-white subtit">SOCIETY</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="rwgnews.html">NEWS</a></li>
+                <li class="nav-item"><a class="nav-link" href="rwgaboutus.html">ABOUT US</a></li>
+                    <li class="nav-item"><a class="nav-link" href="executiveboard.html">EXECUTIVE BOARD</a></li>
+                    <li class="nav-item"><a class="nav-link" href="becomingmember.html">BECOME A MEMBER</a></li>
+              </ul>
+              </div>
+              <!-- /.col-md-4  -->
+              <div class="col">
+               <div class="text-uppercase text-white subtit">RUDOLF WOLF</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="biography.html">BIOGRAPHY</a></li>
+                <li class="nav-item"><a class="nav-link" href="rwpubl.html">PUBLICATIONS</a></li>
+              </ul>
+              </div>
+             <!-- /.col-md-4  -->
+              <div class="col">
+               <div class="text-uppercase text-white subtit">PRACTICE GROUP</div>
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="pgaboutus.html">ABOUT US</a></li>
+                <li class="nav-item"><a class="nav-link" href="workarea.html">WORK AREAS</a></li>
+                <li class="nav-item"><a class="nav-link" href="rwsactivities.html">ACTIVITIES</a></li>
+                <li class="nav-item"><a class="nav-link" href="eventcal.html">EVENT CALENDAR</a></li>
+                <li class="nav-item"><a class="nav-link" href="eventarchive.html">EVENT ARCHIVE</a></li>
+                <li class="nav-item"><a class="nav-link" href="becomesolobs.html">BECOME A SOLAR OBSERVER</a></li>
+              </ul>
+              </div>
+            </div>
+          </div>
+          <!--  /.container  -->
+
+
+        </div>
+      </li>
+    </ul>
+    <!--<form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>-->
+  </div>
+
+<!--</div>-->
+</nav>
+
